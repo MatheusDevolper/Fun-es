b5cd3930b96851c8380d5f9022acfa6e683f73e5
Dra.Telma Sá <matheusps1407@gmail.com>
2021-01-06 17:51:50 -0300

    Projeto inciado

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _20_11_Funções
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Saudacao();
+
+            Console.WriteLine("Digite uma operação");
+            string operacao = Console.ReadLine();
+
+            Console.WriteLine("Digite o primeiro numero");
+            float num1= float.Parse( Console.ReadLine() ) ;
+
+            Console.WriteLine("Digite o segundo numero");
+            float num2 = float.Parse( Console.ReadLine() ) ;
+
+
+            switch (operacao)
+            {
+                case "Soma":
+                // var resultado = num1 + num2;
+                Soma(num1, num2);
+                break;
+
+            case "Subtrair":
+                Subtrair(num1, num2);
+                break;
+
+            default:
+                break;
+            }
+
+            string[] nomes = {"Carlos", "Paulo", "Pricila", "Lemos", "Thiago"};
+
+            ListarProfessores( nomes );
+        }
+    }
+}
